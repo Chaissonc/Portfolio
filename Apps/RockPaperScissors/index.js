2d9376c9c47458ba5fef0fdcd5fbd48bb7806c82
Chaisson <Chaissonrcook@gmail.com>
2024-11-12 20:01:07 -0800

    Update index.js

--- Apps/RockPaperScissors/index.js
+++ Apps/RockPaperScissors/index.js
@@ -1,7 +1,3 @@
-/*Hello World! this is the JavaScript file that makes 
-everything work on the rock paper scissors game. 
-everything is open source so you can have a look!*/
-
 const choices = ["Rock","Paper","Scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
